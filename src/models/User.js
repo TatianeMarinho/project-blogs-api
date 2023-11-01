1fb89c5a134c614c4961ef85dcd788195e37605e
TatianeMarinho <devtatianemarinho@gmail.com>
2023-11-01 10:52:51 -0400

    modelo User em src/models/User.js

---
+++ src/models/User.js
@@ -0,0 +1,20 @@
+const UserTableSchema = (sequelize, DataTypes) => {
+    const UserTable = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+        },
+        displayName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        image: DataTypes.STRING,
+    }, {
+        tableName: 'users',
+        underscored: true,
+        timestamps: false,
+    });
+
+    return UserTable;
+}
+
+module.exports = UserTableSchema;
