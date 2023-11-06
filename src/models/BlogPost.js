0c28a08e9d755432c8d6f6dc526b94f0891a08bc
TatianeMarinho <devtatianemarinho@gmail.com>
2023-11-06 17:19:49 -0400

    model BlogPost

---
+++ src/models/BlogPost.js
@@ -0,0 +1,32 @@
+const BlogPostTableSchema = (sequelize, DataTypes) => {
+    const BlogPostTable = sequelize.define('BlogPost', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      userId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      },
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
+    }, {
+      tableName: 'blog_posts',
+      underscored: true,
+      timestamps: false
+    });
+  
+    BlogPostTable.associate = ({ User }) => {
+      BlogPostTable.belongsTo(User, {
+        foreignKey: 'userId',
+        as: 'User',
+      })
+    }
+  
+    return BlogPostTable;
+  }
+  
+  module.exports = BlogPostTableSchema;
--- src/models/User.js
+++ src/models/User.js
@@ -1,20 +1,28 @@
 const UserTableSchema = (sequelize, DataTypes) => {
     const UserTable = sequelize.define('User', {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-        },
-        displayName: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING,
-        image: DataTypes.STRING,
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
     }, {
-        tableName: 'users',
-        underscored: true,
-        timestamps: false,
+      tableName: 'users',
+      underscored: true,
+      timestamps: false
     });
-
+  
+    UserTable.associate = ({BlogPost}) => {
+      UserTable.hasMany(BlogPost, {
+        foreignKey: 'userId',
+        as: 'blog_posts',
+      })
+    };
+  
     return UserTable;
-}
-
-module.exports = UserTableSchema;
+  }
+  
+  module.exports = UserTableSchema;
